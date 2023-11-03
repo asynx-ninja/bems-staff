f947c08881ea8993276a4ff5a8bfd1887d2619e2
Hooseun <rimzeljhnbatallones@gmail.com>
2023-11-03 19:52:22 +0800

    archived announcement

--- src/components/archivedAnnouncement/Breadcrums.jsx
+++ src/components/archivedAnnouncement/Breadcrums.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
+import { GrFormNext } from "react-icons/gr";
+import { TfiAnnouncement } from "react-icons/tfi";
 
-const Breadcrumb = () => {
+const Breadcrumbs = () => {
   return (
     <nav className="flex ">
       <ol className="flex items-center space-x-2 text-gray-500 mt-[-1rem]">
+        <TfiAnnouncement size={22} />
         <li>
-          <a href="/announcements" className="text-gray-900 hover:underline">Announcement</a>
+          <a href="/announcements" className="text-gray-600 font-bold hover:underline">
+            ANNOUNCEMENT
+          </a>
         </li>
         <li>
-          <span>/</span>
+          <GrFormNext size={24} style={{ color: "#ffffff" }} />
         </li>
-        <li className="text-teal-600">Archived Announcement</li>
+        <li className="text-[#295141] font-bold">ARCHIVED ANNOUNCEMENT</li>
       </ol>
     </nav>
   );
 };
 
-export default Breadcrumb;
+export default Breadcrumbs;
