2abdd74e3ee1334f28680a0efaa285180bf56363
NyleChua <nyle.chua05@gmail.com>
2023-11-03 20:33:44 +0800

    staff frontend completed - merged with rimel

--- src/pages/ArchivedRequests.jsx
+++ src/pages/ArchivedRequests.jsx
@@ -229,7 +229,7 @@ const ArchivedRequests = () => {
                       className="sm:hidden md:block hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-20 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm "
                       role="tooltip"
                     >
-                      Restore Selected Announcement
+                      Restore Selected Requests
                     </span>
                   </button>
                 </div>
--- src/pages/Officials.jsx
+++ src/pages/Officials.jsx
@@ -122,13 +122,13 @@ const Officials = () => {
                   >
                     <FaPlus size={24} style={{ color: "#ffffff" }} />
                     <span className="sm:block md:hidden sm:pl-5">
-                      Add Services
+                      Add Official
                     </span>
                     <span
                       className="sm:hidden md:block hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-20 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm "
                       role="tooltip"
                     >
-                      Add Services
+                      Add Official
                     </span>
                   </button>
                 </div>
@@ -253,7 +253,7 @@ const Officials = () => {
                       className="sm:hidden md:block hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-20 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded-md shadow-sm "
                       role="tooltip"
                     >
-                      Archive Selected Requests
+                      Archive Selected Officials
                     </span>
                   </button>
                 </div>
